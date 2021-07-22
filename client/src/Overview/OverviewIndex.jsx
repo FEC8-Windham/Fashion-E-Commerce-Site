1fba087c1bf36f5beff7329e8bc44a357ca4fb61
mikeko <geonhee.ko@gmail.com>
2021-07-22 16:24:52 -0400

    create config.js

--- client/src/Overview/OverviewIndex.jsx
+++ client/src/Overview/OverviewIndex.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import StarRating from '../Helper-Components/StarRating.jsx';
+import axios from 'axios';
 
 class OverviewIndex extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  getInfo() {
+    axios.get('/user?ID=12345')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+
   }
 
   render() {
---
+++ client/src/Overview/ProductInfo 2.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class ProductInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        Realted Products and My Outfit Component!
+      </div>
+    )
+  }
+}
+
+export default ProductInfo;
