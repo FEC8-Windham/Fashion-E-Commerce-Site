d74da4bdd4a9f52d46b82a253894c71b6f8ee6bb
Hayden Bonnen <HaydenBP@Gmail.com>
2021-07-23 13:42:38 -0400

    fixed import error

--- client/src/Review/ReviewTile.jsx
+++ client/src/Review/ReviewTile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import StarRating from '../../Helper-Components/StarRating.jsx';
+import StarRating from '../Helper-Components/StarRating.jsx';
 import { Container, FlexContainer } from './Styles/ReviewStyles.js';
 
 
@@ -31,25 +31,3 @@ const ReviewTile = () => {
 };
 
 export default ReviewTile;
-
-
-
-// <Container height='100%' width='100%' bgColor={'blue'}>
-// <Container height='15%' width='95%' padding='5px' bgColor={'green'}>
-//   <Container height='100%' width='50%' float='left' bgColor={'purple'}>
-//     <span>Stars</span>
-//   </Container>
-//   <Container height='100%' width='50%' float='right' bgColor={'yellow'}>
-//     <span>User</span>
-//   </Container>
-// </Container>
-// <Container height='15%' width='100%' bgColor={'pink'}>
-//   <span>Title</span>
-// </Container>
-// <Container height='45%' width='100%' bgColor={'orange'}>
-//   <span>Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body</span>
-// </Container>
-// <Container height='10%' width='100%' bgColor={'plum'}>
-//   <span>Helpful widget</span>
-// </Container>
-// </Container>
