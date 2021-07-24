e056e3faf6e3c7338576da1b270f966567d76727
mikeko <geonhee.ko@gmail.com>
2021-07-24 13:15:16 -0400

    Make currentStyle and averageRating available in App.js

--- client/src/Overview/ProductInfo/ProductInfo.jsx
+++ client/src/Overview/ProductInfo/ProductInfo.jsx
@@ -4,16 +4,14 @@ import StarRating from '../../Helper-Components/StarRating.jsx';
 import Styles from './Styles.jsx';
 import { Category, Name, Price, Style, Size, Description } from '../Styled-Components/Styled-ProductInfo.js';
 
-var ProductInfo = ({rating, category, name, price, styles, size}) => {
-
-
+var ProductInfo = ({rating, category, name, price, styles, size, setCurrStyle, currStyle}) => {
   return (
     <div>
       <StarRating rating={rating}/>
       <Category>{category}</Category>
       <Name>{name}</Name>
       <Price>${price}</Price>
-      <Styles styles = {styles}/>
+      <Styles styles = {styles} setCurrStyle ={setCurrStyle} currStyle = {currStyle}/>
       <Size>{size}</Size>
       <Description></Description>
     </div>
