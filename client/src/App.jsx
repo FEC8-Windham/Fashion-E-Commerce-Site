6708b4fcb3ec07835c60bf814f9bb4bd18ad0ec1
mikeko <geonhee.ko@gmail.com>
2021-07-24 16:05:06 -0400

    Render the price

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,45 @@ import OutfitIndex from './Outfit/OutfitIndex.jsx';
 import ReviewIndex from './Review/ReviewIndex.jsx';
 import axios from 'axios';
 import { getData } from './Controllers/getData.js';
-
+import { calculateAverageRating, getDefaultStyle } from './HelperFunctions.js';
 
 
 var App = (props) => {
 
   var [loaded, setLoaded] = useState(false);
-  var [metaData, setMetaData] = useState({});
+  var [metaData, setMetaData] = useState(null);
   var [currStyle, setCurrStyle] = useState(null);
   var [averageRating, setAverageRating] = useState(0);
+  var [originalPrice, setOriginalPrice] = useState(0);
+  var [salePrice, setSalePrice] = useState(0);
 
+
+  //getting all the data at once.
   useEffect( async () => {
     setMetaData(await getData());
     setLoaded(true);
   }, []);
 
+
+  useEffect(() => {
+    //Following will be invoked when currentStyle changes.
+    if (currStyle) {
+      setOriginalPrice(currStyle.original_price);
+      setSalePrice(currStyle.sale_price);
+    }
+  }, [currStyle]);
+
+  //Set the current style as default and average rating
+  useEffect(()=> {
+    if (metaData) {
+      setCurrStyle(getDefaultStyle(metaData.productStyles));
+      setAverageRating(calculateAverageRating(metaData.reviewMeta.ratings));
+    }
+  }, [metaData]);
+
   return (
     <div>
-      {loaded ? <OverviewIndex productInfo = {metaData.productInfo} productStyles = {metaData.productStyles} reviewMeta = {metaData.reviewMeta} currStyle = {currStyle}setCurrStyle = {setCurrStyle} setAverageRating ={setAverageRating} averageRating= {averageRating}/> : <div>Loading...</div>}
+      {loaded ? <OverviewIndex productInfo = {metaData.productInfo} productStyles = {metaData.productStyles} currStyle = {currStyle} setCurrStyle = {setCurrStyle} averageRating= {averageRating} originalPrice = {originalPrice} salePrice = {salePrice}/> : <div>Loading...</div>}
 
       <div><OutfitIndex /></div>
       <div><ReviewIndex/></div>
--- client/src/Overview/Styled-Components/Styled-ProductInfo.js
+++ client/src/Overview/Styled-Components/Styled-ProductInfo.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export var Star = styled.span`
+  background: linear-gradient(90deg, gold ${(props) => props.yellowPercentage}%, darkgray ${(props) => props.yellowPercentage}% ${props => 100 - props.yellowPercentage}%);
+  color:black;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+
 export var Category = styled.div`
 
 
@@ -12,24 +20,38 @@ export var Name = styled.div`
 
 `;
 
-
 export var Price = styled.div`
-
-
 
 `;
 
+
+export var OriginalPrice = styled.div`
+// if props is true, cross it out
+float:left;
+margin-right:15px;
+text-decoration: ${props => props.sale ? 'line-through' : 'none'};
+`;
+
+export var SalePrice = styled.div`
+color: red;
+
+`;
+
+
 export var StyleThumbnail = styled.img`
   border-radius: 50%;
-  width:30px;
-  height: 30px;
+  width:40px;
+  height: 40px;
   padding: 5px;
+  float: left;
 
 `;
 
 export var StylesContainer = styled.div`
-  float: left;
+  width:200px;
+  height: 120px;
 `;
+
 
 export var Size = styled.div`
 
