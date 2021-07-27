01c9fe7d6d2dffc57da98987a1d69053138b3c3c
Seiji Matsumoto <seijim27@gmail.com>
2021-07-27 14:28:02 -0400

    Styled modal properly with dummy data

--- client/src/Outfit/ComparisonModal.jsx
+++ client/src/Outfit/ComparisonModal.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Modal, Data, CloseBtn } from './styles/Modal.style';
+import {
+  Modal, Data, CloseBtn,
+  Table, Title, ProductNames, Row, Column1, Column2, Column3
+} from './styles/Modal.style';
 
 const ComparisonModal = ({ message, isOpen, onClose, children }) => {
   if (!isOpen) {
@@ -8,8 +11,48 @@ const ComparisonModal = ({ message, isOpen, onClose, children }) => {
   }
   return ReactDOM.createPortal(
     <Modal>
-      <Data>{message}</Data>
-      <CloseBtn onClick={onClose}>Close</CloseBtn>
+      <Data>
+        <Title>Comparing</Title>
+        <Table>
+          <tbody>
+            <Row>
+              <ProductNames>{message}</ProductNames>
+              <ProductNames align={'right'}>Current product</ProductNames>
+            </Row>
+          </tbody>
+        </Table>
+        <br></br>
+        <Table>
+          <tbody>
+            <Row>
+              <Column1>✓</Column1>
+              <Column2>GMO and Pesticide-free</Column2>
+              <Column3></Column3>
+            </Row>
+            <Row>
+              <Column1></Column1>
+              <Column2>Made with 100% Genetic Modification</Column2>
+              <Column3>✓</Column3>
+            </Row>
+            <Row>
+              <Column1>✓</Column1>
+              <Column2>This is made up</Column2>
+              <Column3></Column3>
+            </Row>
+            <Row>
+              <Column1>✓</Column1>
+              <Column2>Feature description</Column2>
+              <Column3>✓</Column3>
+            </Row>
+            <Row>
+              <Column1>✓</Column1>
+              <Column2>Uses React Hooks</Column2>
+              <Column3></Column3>
+            </Row>
+          </tbody>
+        </Table>
+        <CloseBtn onClick={onClose}>Close</CloseBtn>
+      </Data>
     </Modal>,
     document.getElementById('modalHere')
   );
--- client/src/Outfit/styles/Modal.style.js
+++ client/src/Outfit/styles/Modal.style.js
@@ -2,20 +2,18 @@ import styled from 'styled-components';
 
 export const Modal = styled.div`
   margin: auto;
-  padding: 20px;
+  padding: 30px;
   left: 0;
   right: 0;
   border-style: solid;
-  border-radius: 5px;
   background: #fff;
-  width: 500px;
-  height: 500px;
+  width: 450px;
   position: absolute;
   z-index: 1;
+  box-shadow: 0px 0px 1px 1px;
 `;
 
 export const Data = styled.span`
-  margin-top: 0;
   color: #333;
   font-family: Tahoma, Arial, sans-serif;
 `;
@@ -24,7 +22,41 @@ export const CloseBtn = styled.button`
   position: absolute;
   top: 20px;
   right: 30px;
-  font-size: 20px;
+  font-size: 15px;
   font-weight: bold;
+  z-index: 1;
   color: #333;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+`;
+
+export const Title = styled.h4`
+  font-weight: bold;
+  padding: 0;
+  padding-left: 3px;
+  margin: 0;
+`;
+
+export const ProductNames = styled.th`
+  width: 50%;
+  text-align: ${prop => prop.align || 'left'};
+`;
+
+export const Column1 = styled.th`
+  width: 10%;
+`;
+
+export const Column2 = styled.th`
+  width: 80%;
+  text-align: center;
+`;
+
+export const Column3 = styled.th`
+  width: 10%;
+`;
+
+export const Row = styled.tr`
+  height: 20px;
 `;
