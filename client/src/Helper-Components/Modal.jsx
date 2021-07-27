b218940da721bf2069770749d45e78e84f109b65
Seiji Matsumoto <seijim27@gmail.com>
2021-07-27 12:03:14 -0400

    Removed shared Modal components

--- client/src/Helper-Components/Modal.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import { Nothing, Background, OverviewModal, ComparisonModalContainer, ComparisonModal, NewReviewModal, ReviewImageModal } from './Modal.style';
-
-const Modal = (component, data) => {
-
-};
-
-export default Modal;
