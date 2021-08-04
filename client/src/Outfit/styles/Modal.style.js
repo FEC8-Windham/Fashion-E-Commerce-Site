4841f998d7c00f7dfd82bb23b33f9653137edd97
Seiji Matsumoto <seijim27@gmail.com>
2021-08-04 19:42:44 -0400

    Fixed modal styling

--- client/src/Outfit/styles/Modal.style.js
+++ client/src/Outfit/styles/Modal.style.js
@@ -8,14 +8,20 @@ export const Modal = styled.div`
   border-style: solid;
   background: #fff;
   position: absolute;
-  width: 45%;
+  width: 500px;
+  height: 250px;
   z-index: 1;
   box-shadow: 0px 0px 1px 1px;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const Data = styled.span`
   color: #333;
   font-family: Tahoma, Arial, sans-serif;
+  overflow: scroll;
 `;
 
 export const CloseBtn = styled.button`
@@ -32,6 +38,7 @@ export const Top = styled.table`
   width: 100%;
 `;
 
+
 export const Table = styled.table`
   width: 100%;
   overflow: scroll;
@@ -40,32 +47,34 @@ export const Table = styled.table`
 export const Title = styled.h4`
   font-weight: normal;
   padding: 0;
-  padding-left: 12px;
   margin: 0;
+  padding-left: 17px;
 `;
 
 export const ProductNames = styled.th`
-  width: ${prop => prop.width || '20%'};
+  width: ${prop => prop.width || '30%'};
   font-size: 18px;
   text-align: center;
 `;
 
-export const Column1 = styled.th`
-  width: 20%;
+export const Column = styled.th`
+  width: 30%;
   font-weight: normal;
   color: ${prop => prop.color || 'black'};
-`;
-
-export const Column2 = styled.th`
-  width: 60%;
+  margin: 0;
   text-align: center;
 `;
 
-export const Column3 = styled.th`
-  width: 20%;
-  font-weight: normal;
+export const MiddleColumn = styled.th`
+  width: 40%;
+  text-align: center;
 `;
 
 export const Row = styled.tr`
   height: 20px;
+`;
+
+export const ColumnStar = styled.div`
+  margin: auto;
+  padding-left: 40px;
 `;
