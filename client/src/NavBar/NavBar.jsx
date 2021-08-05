724711c32cc2601ac41433bb3cbc268702632134
Seiji Matsumoto <seijim27@gmail.com>
2021-08-05 19:32:14 -0400

    changed

--- client/src/NavBar/NavBar.jsx
+++ client/src/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <Nav>
       <NavList>
-        <NavItems><NavLinks href='#moduleOverview'>Overview</NavLinks></NavItems>
+        <NavItems><NavLinks href='#'>Overview</NavLinks></NavItems>
         <NavItems><NavLinks href='#moduleOutfit'>Outfits</NavLinks></NavItems>
         <NavItems><NavLinks href='#moduleReview'>Reviews</NavLinks></NavItems>
       </NavList>
